0f9a256c7fbe8707bdca0afb050dd8d4595cf0a1
佐々木　謙 <nkekisasa222@gmail.com>
2022-02-26 12:54:39 +0900

    Initial Commit

---
+++ CleanArchitectureTraining/CleanArchitectureTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureTrainingApp.swift
+//  CleanArchitectureTraining
+//
+//  Created by sasaki.ken on 2022/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
