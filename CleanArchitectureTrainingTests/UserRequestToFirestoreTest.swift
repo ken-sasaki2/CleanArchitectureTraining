9b8b7446ded1983c41751869094c903923e7d31d
佐々木　謙 <nkekisasa222@gmail.com>
2022-03-06 16:31:15 +0900

    test: testSaveUser書いた

---
+++ CleanArchitectureTrainingTests/UserRequestToFirestoreTest.swift
@@ -0,0 +1,31 @@
+//
+//  UserRequestToFirestoreTest.swift
+//  CleanArchitectureTrainingTests
+//
+//  Created by sasaki.ken on 2022/03/06.
+//
+
+import XCTest
+
+class UserRequestToFirestoreTest: XCTestCase {
+    private let firestore = UserRequestToFirestore()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSaveUser() async throws {
+        let inputData = UserAddInputData(name: "sasaki.ken", gender: 1, birthday: "1996年2月22日", createdAt: Date().timeIntervalSince1970)
+        let inputEntity = UserAddInputEntity(inputData: inputData)
+        do {
+            try await firestore.saveUser(inputEntity: inputEntity)
+            print("Success test save user.")
+        } catch {
+            XCTFail("Fail test save user.")
+        }
+    }
+}
