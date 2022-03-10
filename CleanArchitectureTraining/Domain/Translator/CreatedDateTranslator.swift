a4cf1025c6ffcd70a4c51064c0bb26d27bbc67b0
佐々木　謙 <nkekisasa222@gmail.com>
2022-03-10 09:38:17 +0900

    update: createdAtからStringへのTranslator書いた

---
+++ CleanArchitectureTraining/Domain/Translator/CreatedDateTranslator.swift
@@ -0,0 +1,23 @@
+//
+//  CreatedDateTranslator.swift
+//  CleanArchitectureTraining
+//
+//  Created by sasaki.ken on 2022/03/09.
+//
+
+import Foundation
+
+final class CreatedDateTranslator {
+    static let shared = CreatedDateTranslator()
+    
+    private init() {}
+    
+    func translate(createdAt: Double) -> String {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
+        formatter.dateFormat = "yyyy年M月d日"
+        let date = formatter.string(from: Date(timeIntervalSince1970: createdAt))
+        
+        return date
+    }
+}
