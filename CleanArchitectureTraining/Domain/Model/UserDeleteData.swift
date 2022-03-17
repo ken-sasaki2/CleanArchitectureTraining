13f98adf2d9d5d3f974d607c587469ec1f41f86d
佐々木　謙 <nkekisasa222@gmail.com>
2022-03-17 23:15:38 +0900

    update: Domain層の削除処理書いた

---
+++ CleanArchitectureTraining/Domain/Model/UserDeleteData.swift
@@ -0,0 +1,16 @@
+//
+//  UserDeleteData.swift
+//  CleanArchitectureTraining
+//
+//  Created by sasaki.ken on 2022/03/17.
+//
+
+import Foundation
+
+struct UserDeleteData {
+    var documentid: String
+    
+    init(documentid: String) {
+        self.documentid = documentid
+    }
+}
---
+++ CleanArchitectureTraining/Domain/UseCase/UserDeleteUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  UserDeleteUseCase.swift
+//  CleanArchitectureTraining
+//
+//  Created by sasaki.ken on 2022/03/17.
+//
+
+import Foundation
+
+protocol UserDeleteUseCaseInterface {
+    func deleteUser(deleteData: UserDeleteData) async throws
+}
+
+final class UserDeleteUseCase: UserDeleteUseCaseInterface {
+    private let userRepository: UserRepositoryInterface
+    private let userDeletePresenter: UserDeletePresenterInterface
+    
+    init(userRepository: UserRepositoryInterface, userDeletePresenter: UserDeletePresenterInterface) {
+        self.userRepository = userRepository
+        self.userDeletePresenter = userDeletePresenter
+    }
+    
+    func deleteUser(deleteData: UserDeleteData) async throws {
+        do {
+            try await userRepository.deleteUser(deleteData: deleteData)
+            userDeletePresenter.successDeleteUser()
+        } catch {
+            userDeletePresenter.failDeleteUser()
+        }
+    }
+}
