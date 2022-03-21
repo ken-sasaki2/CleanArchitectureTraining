e7b7b717915ef090a7a46987fb732c8c396d18b6
佐々木　謙 <nkekisasa222@gmail.com>
2022-03-21 17:19:12 +0900

    update: datestore書いた

---
+++ CleanArchitectureTraining/Data/DataStore/AuthDataStore.swift
@@ -0,0 +1,21 @@
+//
+//  AuthDataStore.swift
+//  CleanArchitectureTraining
+//
+//  Created by sasaki.ken on 2022/03/20.
+//
+
+import Foundation
+
+protocol AuthDataStoreInterface {
+    
+}
+
+final class AuthDataStore: AuthDataStoreInterface {
+    private let authRequest = AuthRequestToFirebase()
+    
+    func createUser(authEntity: AuthCreateUserEntity) async -> AuthResponseTypeEntity {
+        let response = await authRequest.createUser(authEntity: authEntity)
+        return response
+    }
+}
