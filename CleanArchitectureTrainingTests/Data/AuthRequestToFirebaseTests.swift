daf39f031bcebc7ef6c3afc58688cb35a5689c08
佐々木　謙 <nkekisasa222@gmail.com>
2022-03-22 22:31:41 +0900

    refactor: CreateUser -> SignUpに変えた

--- CleanArchitectureTrainingTests/Data/AuthRequestToFirebaseTests.swift
+++ CleanArchitectureTrainingTests/Data/AuthRequestToFirebaseTests.swift
@@ -20,29 +20,29 @@ class AuthRequestToFirebaseTests: XCTestCase {
     
     func testCreateUser_responseIsSuccess() async throws {
         try XCTSkipIf(true, "Firebaseに登録済みの場合テストが失敗するのでスキップ")
-        let authEntity = AuthCreateUserEntity(email: TestHelper.email, password: TestHelper.password)
-        let response = await authRequestToFirebase.createUser(authEntity: authEntity)
+        let authEntity = AuthSignUpEntity(email: TestHelper.email, password: TestHelper.password)
+        let response = await authRequestToFirebase.signUp(signUpEntity: authEntity)
         print("Success test create user.")
         XCTAssert(response == .success)
     }
     
     func testCreateUser_responseIsInvalidEmail() async throws {
-        let authEntity = AuthCreateUserEntity(email: "test_user@", password: TestHelper.password)
-        let response = await authRequestToFirebase.createUser(authEntity: authEntity)
+        let authEntity = AuthSignUpEntity(email: "test_user@", password: TestHelper.password)
+        let response = await authRequestToFirebase.signUp(signUpEntity: authEntity)
         print("Fail invalid email.")
         XCTAssert(response == .invalidEmail)
     }
     
     func testCreateUser_responseIsWeakPassword() async throws {
-        let authEntity = AuthCreateUserEntity(email: TestHelper.email, password: "xxxxx")
-        let response = await authRequestToFirebase.createUser(authEntity: authEntity)
+        let authEntity = AuthSignUpEntity(email: TestHelper.email, password: "xxxxx")
+        let response = await authRequestToFirebase.signUp(signUpEntity: authEntity)
         print("Fail weak password.")
         XCTAssert(response == .weakPassword)
     }
     
     func testCreateUser_responseIsEmailAlreadyInUse() async throws {
-        let authEntity = AuthCreateUserEntity(email: "email_already_in_use@example.com", password: TestHelper.password)
-        let response = await authRequestToFirebase.createUser(authEntity: authEntity)
+        let authEntity = AuthSignUpEntity(email: "email_already_in_use@example.com", password: TestHelper.password)
+        let response = await authRequestToFirebase.signUp(signUpEntity: authEntity)
         print("Fail email already in use.")
         XCTAssert(response == .emailAlreadyInUse)
     }
