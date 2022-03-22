7f5e163e86f7cea9223e1d5a1ad4b5c3997fa98b
佐々木　謙 <nkekisasa222@gmail.com>
2022-03-22 23:05:10 +0900

    update: AuthSignUpView書いた

--- CleanArchitectureTraining/Presentation/UI/View/AuthSignUpView.swift
+++ CleanArchitectureTraining/Presentation/UI/View/AuthSignUpView.swift
@@ -11,53 +11,89 @@ struct AuthSignUpView: View {
     @ObservedObject var authSignUpVM: AuthSignUpViewModel
     @State private var email = ""
     @State private var password = ""
+    @State private var signUpButtonEnabled = false
     let authController: AuthController
     
     var body: some View {
-        ButtonView(text: "Sign Up.", color: .black, buttonEnabled: true) {
-            onTapSignUpButton(email: "nkekisasa222@gmail.com", password: "nkekisasa2")
-        }
-        .alert("登録完了", isPresented: $authSignUpVM.isShowSuccessSignUpAlert) {
-            Button("OK") {
-                //ページ遷移
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                Text("新規ユーザー登録")
+                    .font(.system(size: 28, weight: .semibold, design: .default))
+                VStack {
+                    VStack(spacing: 0) {
+                        Text("必須")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 12, weight: .regular, design: .default))
+                            .frame(width: geometry.size.width / 1.3, alignment: .leading)
+                        TextField("メールアドレス（6文字以上）", text: $email)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, 35)
+                            .keyboardType(.default)
+                    }
+                    VStack(spacing: 0) {
+                        Text("必須")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 12, weight: .regular, design: .default))
+                            .frame(width: geometry.size.width / 1.3, alignment: .leading)
+                        TextField("パスワード", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, 35)
+                            .keyboardType(.default)
+                    }
+                }
+                .padding(.top, 20)
+                ButtonView(text: "サインアップ", color: .black, buttonEnabled: true) {
+                    onTapSignUpButton(email: email, password: password)
+                }
+                .padding(.top, 20)
+                
+                .alert("登録完了", isPresented: $authSignUpVM.isShowSuccessSignUpAlert) {
+                    Button("OK") {
+                        //ページ遷移
+                    }
+                } message: {
+                    Text("サインアップに成功しました！")
+                }
+                .alert("登録失敗", isPresented: $authSignUpVM.isShowFailInvalidEmailAlert) {
+                    Button("やり直す") {
+                        resetEmailAndPassword()
+                    }
+                } message: {
+                    Text("メールアドレスの形式が間違っています")
+                }
+                .alert("登録失敗", isPresented: $authSignUpVM.isShowFailWeakPasswordAlert) {
+                    Button("やり直す") {
+                        resetEmailAndPassword()
+                    }
+                } message: {
+                    Text("パスワードは6文字以上で入力して下さい")
+                }
+                .alert("登録失敗", isPresented: $authSignUpVM.isShowFailEmailAlreadyInUseAlert) {
+                    Button("やり直す") {
+                        resetEmailAndPassword()
+                    }
+                } message: {
+                    Text("このメールアドレスは既に登録されています")
+                }
+                .alert("登録失敗", isPresented: $authSignUpVM.isShowFailNetworkErrorAlert) {
+                    Button("OK") {
+                        resetEmailAndPassword()
+                    }
+                } message: {
+                    Text("登録に失敗しました。通信状態が良好な環境で再度お試しください")
+                }
+                .alert("登録失敗", isPresented: $authSignUpVM.isShowFailOtherErrorAlert) {
+                    Button("OK") {
+                        resetEmailAndPassword()
+                    }
+                } message: {
+                    Text("入力内容が正しくありません")
+                }
+                Spacer()
             }
-        } message: {
-            Text("サインアップに成功しました！")
-        }
-        .alert("登録失敗", isPresented: $authSignUpVM.isShowFailInvalidEmailAlert) {
-            Button("やり直す") {
-                resetEmailAndPassword()
-            }
-        } message: {
-            Text("メールアドレスの形式が間違っています")
-        }
-        .alert("登録失敗", isPresented: $authSignUpVM.isShowFailWeakPasswordAlert) {
-            Button("やり直す") {
-                resetEmailAndPassword()
-            }
-        } message: {
-            Text("パスワードは6文字以上で入力して下さい")
-        }
-        .alert("登録失敗", isPresented: $authSignUpVM.isShowFailEmailAlreadyInUseAlert) {
-            Button("やり直す") {
-                resetEmailAndPassword()
-            }
-        } message: {
-            Text("このメールアドレスは既に登録されています")
-        }
-        .alert("登録失敗", isPresented: $authSignUpVM.isShowFailNetworkErrorAlert) {
-            Button("OK") {
-                resetEmailAndPassword()
-            }
-        } message: {
-            Text("登録に失敗しました。通信状態が良好な環境で再度お試しください")
-        }
-        .alert("登録失敗", isPresented: $authSignUpVM.isShowFailOtherErrorAlert) {
-            Button("OK") {
-                resetEmailAndPassword()
-            }
-        } message: {
-            Text("登録に失敗しました。時間をあけて再度お試しください")
         }
     }
 }
