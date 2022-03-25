509c63312890cdd674423ee064ad3caa8440b2d3
佐々木　謙 <nkekisasa222@gmail.com>
2022-03-25 17:30:07 +0900

    update: 消えたUserFetchOutputDataを作り直し

---
+++ CleanArchitectureTraining/Domain/Model/UserFetchOutputData.swift
@@ -0,0 +1,22 @@
+//
+//  UserFetchOutputData.swift
+//  CleanArchitectureTraining
+//
+//  Created by sasaki.ken on 2022/03/25.
+//
+
+import Foundation
+
+struct UserFetchOutputData {
+    var name: String
+    var gender: String
+    var createdDay: String
+    var documentId: String
+    
+    init(name: String, gender: String, createdDay: String, documentId: String) {
+        self.name = name
+        self.gender = gender
+        self.createdDay = createdDay
+        self.documentId = documentId
+    }
+}
